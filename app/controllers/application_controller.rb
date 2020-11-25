be128a55520bceecf83bc348e115b5f9573ea476
Pauline Chane <pauline.chane@gmail.com>
2020-11-24 22:39:21 -0800

    controller filter for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,17 @@ class ApplicationController < ActionController::Base
       @login_user = User.find_by(id: session[:user_id])
     end
   end
+
+  before_action :require_login
+
+  def current_user
+    @current_user = User.find(session[:user_id])
+  end
+
+  def require_login
+    if current_user.nil?
+      flash[:error] = "You must be logged in to view this section"
+      redirect_to login_path
+    end
+  end
 end
