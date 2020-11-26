4d3cd0a9f0872f7a6504dcd8464aacbdf81ea3f1
Pauline Chane <pauline.chane@gmail.com>
2020-11-25 21:14:54 -0800

    added works created by user to views and redid db seeds to load to default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
 require "csv"
 media_file = Rails.root.join("db", "media_seeds.csv")
+# hosts the existing database with a default valid user that is essentially owned by the site itself
+default_user = User.create!(provider: 'github',
+                            uid: "000000000000000000000",
+                            name: "MediaRankerDatabase",
+                            email: "database@mediaranker.com")
 
 CSV.foreach(media_file, headers: true, header_converters: :symbol, converters: :all) do |row|
   data = Hash[row.headers.zip(row.fields)]
   puts data
-  Work.create!(data)
+  default_user.works.create!(data)
 end
