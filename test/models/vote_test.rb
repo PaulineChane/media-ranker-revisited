bb5af1c5c1a2d322adc0a0e5525d47a99daa333d
Pauline Chane <pauline.chane@gmail.com>
2020-11-25 20:13:31 -0800

    fixed vote model tests, need to fix one more set of tests and seeds

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -16,10 +16,10 @@ describe Vote do
   end
 
   describe "validations" do
-    let (:user1) { User.new(username: "chris") }
-    let (:user2) { User.new(username: "chris") }
-    let (:work1) { Work.new(category: "book", title: "House of Leaves") }
-    let (:work2) { Work.new(category: "book", title: "For Whom the Bell Tolls") }
+    let (:user1) { User.new(provider: "google_oauth2", uid: "090909090909", username: "chris", email: "chris@chris.com") }
+    let (:user2) {  User.new(provider: "google_oauth2", uid: "090909090903", username: "luda", email: "luda@chris.com")  }
+    let (:work1) { users(:dan).works.new(category: "book", title: "House of Leaves") }
+    let (:work2) { users(:dan).works.new(category: "book", title: "For Whom the Bell Tolls") }
 
     it "allows one user to vote for multiple works" do
       vote1 = Vote.new(user: user1, work: work1)
